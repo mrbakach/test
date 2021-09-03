de5e8ee647019ca51df6478edf478cf992485ae5
Max Bakach <mrbakach@gmail.com>
2021-09-03 15:52:38 +0300

    test

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, this is awesome. Hellooooooooooooooooooo!!")
+	fmt.Fprintf(w, "Hello, this is awesome. Helloooooooooooooo!!")
 }
